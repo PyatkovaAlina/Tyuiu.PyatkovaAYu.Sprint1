991c8ca5e1338e6bcf1c929105f311660f1a9a83
Tyuiu.PyatkovaAYu@outlook.com <Tyuiu.PyatkovaAYu@outlook.com>
2023-09-26 00:03:20 +0500

    Task4

---
+++ Tyuiu.PyatkovaAYu.Sprint1.Task4.V21.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.PyatkovaAYu.Sprint1.Task4.V21.Lib
+{
+    public class DataService : ISprint1Task4V21
+    {
+        public double Calculate(double x, double y)
+        {
+            var res = Math.Round((Math.Abs(Math.Pow(1 + x, 2) - y)) / (x + y), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.PyatkovaAYu.Sprint1.Task4.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PyatkovaAYu.Sprint1.Task4.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PyatkovaAYu.Sprint1.Task4.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c36f53d3-5e0d-440b-8c62-1fc0b1190fa4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
